f7f3dd4e8edff8d10a30bb09a353564340028884
zakiyullah603-sys <zakiyullah603@gmail.com>
2025-11-16 22:03:32 +0800

    Update uas.cpp

--- uas.cpp
+++ uas.cpp
@@ -15,3 +15,22 @@ string namaMenu[JUMLAH_MENU] = {
 
 int hargaMenu[JUMLAH_MENU] = {25000, 20000, 18000, 5000, 10000};
 int jumlahPesanan[JUMLAH_MENU] = {0};
+// =============================
+//       FUNGSI-FUNGSI
+// =============================
+
+void tampilkanHeader() {
+    cout << "=====================================\n";
+    cout << "   SISTEM PEMESANAN RESTORAN - DineXpress\n";
+    cout << "=====================================\n";
+}
+
+void tampilkanMenu() {
+    cout << "\n=============================\n";
+    cout << "     MENU RESTORAN DineXpress\n";
+    cout << "=============================\n";
+    for (int i = 0; i < JUMLAH_MENU; i++) {
+        cout << i + 1 << ". " << namaMenu[i] << " - Rp " << hargaMenu[i] << endl;
+    }
+    cout << "=============================\n";
+}
