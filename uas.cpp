9d4aafb5847d0d4ddefcd6bada00aa5be2efafe9
andiishak2007-a11y <andiishak2007@gmail.com>
2025-11-16 22:01:19 +0800

    Update uas.cpp

--- uas.cpp
+++ uas.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+const int JUMLAH_MENU = 5;
+
+// Data menu restoran
+string namaMenu[JUMLAH_MENU] = {
+    "Nasi Goreng Spesial",
+    "Ayam Geprek Sambal Ijo",
+    "Mie Ayam Komplit",
+    "Es Teh Manis",
+    "Jus Alpukat"
+};
+
+int hargaMenu[JUMLAH_MENU] = {25000, 20000, 18000, 5000, 10000};
+int jumlahPesanan[JUMLAH_MENU] = {0};
